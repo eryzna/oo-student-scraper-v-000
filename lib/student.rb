cbd74b2d55893f843e02c903ba1d78ed6f22caef
Elana Ryznar <eryzna@gmail.com>
2018-09-17 07:56:50 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,12 +37,12 @@ class Student
 
   end
 
-  #def add_student_attributes(attributes_hash)
+  def add_student_attributes#(attributes_hash)
    #attributes_hash.each do |attr, value|
      #self.send("#{attr}=", value)
   # end
   # self
-#  end
+  end
 
   def self.all
 
