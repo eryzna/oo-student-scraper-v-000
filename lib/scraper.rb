62652aab0c629c7c02d59f76c1e01d71cef44f9f
Elana Ryznar <eryzna@gmail.com>
2018-09-17 05:37:33 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -37,7 +37,7 @@ class Scraper
       end
     end
     student[:profile_quote]=doc.css(".profile-quote").text
-    cats=doc.css("div.bio-content.content-holder div.description-holder p").text
+    student[:bio]=doc.css("div.bio-content.content-holder div.description-holder p").text
     #profile_page.css("div.bio-content.content-holder div.description-holder p").text
     binding.pry
   end
